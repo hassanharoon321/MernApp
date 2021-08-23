c7cc264a645ffb1353d76d9a1396ab730e44020b
hassan haroon <hassanharoon321@gmail.com>
2021-08-23 23:51:32 +0500

    added

--- src/User/Pages/Users.js
+++ src/User/Pages/Users.js
@@ -11,6 +11,12 @@ function Users() {
         },
         {
             id: "u2",
+            name: "Hassan Haroon",
+            image: "https://media-exp1.licdn.com/dms/image/C5603AQEUoNaCxGdRsA/profile-displayphoto-shrink_200_200/0/1565136033018?e=1634774400&v=beta&t=shhL1IUwSORi1USqeYwopiw1XPj2wAW3T3K1wb1ixWg",
+            places: 3
+        },
+        {
+            id: "u2",
             name: "Haroon Khan",
             image: "https://media-exp1.licdn.com/dms/image/C5603AQEUoNaCxGdRsA/profile-displayphoto-shrink_200_200/0/1565136033018?e=1634774400&v=beta&t=shhL1IUwSORi1USqeYwopiw1XPj2wAW3T3K1wb1ixWg",
             places: 3
